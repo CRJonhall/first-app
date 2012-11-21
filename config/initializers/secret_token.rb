74482843b95b60861ff91f5cf313d84336e1b1d6
Jon Hall <jon.hall@cloudreach.co.uk>
2012-11-21 11:51:42 +0000

    First commit of base project folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7f8145cf21b4cde7bfea91c21b862e1ac80e8402ef411314db65df84039b28fe8b289d53d4ce16d66cd878d7143429fe299db5870669bc76211220e3c0ee358e'
